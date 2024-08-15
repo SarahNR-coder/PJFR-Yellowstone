a5cbe96096ad5b21f3dc7e2826640bad884317e3
SarahRichou <85622707+SarahNR-coder@users.noreply.github.com>
2024-08-15 10:22:59 +0200

    Fonctionnalité menu nav réussie

--- main.js
+++ main.js
@@ -6,24 +6,40 @@ console.log(toggleTable);
 
 let isDisplayed = false;
 hamburgerIconElement.addEventListener("click", function(){
-    if(window.screen.width <=800){
-        if(isDisplayed === true){
-            toggleTable.map((element)=>{
-                element.computedStyleMap.display = "none";
-            })
-        }else{
-            toggleTable.map((element)=>{
-                element.computedStyleMap.display = "block";
-            })
-        }
+    console.log("Je passe dans l'eventlistener du click sur l'élément de classe 'icon'");
+    if(isDisplayed === true){
+        console.log("je passe dans le if isDisplayed vaut true");
+        toggleTable.map((element)=>{
+            element.style.display = "none";
+        })
     }else{
         toggleTable.map((element)=>{
-            element.computedStyleMap.display = "block";
+            element.style.display = "block";
         })
     }
     isDisplayed= !isDisplayed;
 });
 
+window.addEventListener("resize", function(){
+    if(window.innerWidth <= 800){
+        if(isDisplayed === true){
+            console.log("je passe dans le if de l'E.L. window au resize, càd quand window.screen.width inf ou egal à 800px");
+            toggleTable.map((element)=>{
+                element.style.display = "block";
+            })
+        }else{
+            console.log("je passe dans le else de l'E.L. window au resize, càd quand window.screen.width sup à 800px");
+            toggleTable.map((element)=>{
+                element.style.display = "none";
+            })
+        }
+    }else{
+        toggleTable.map((element)=>{
+            element.style.display = "block";
+        })
+        isDisplayed = false;
+    }
+})
 
 
 
