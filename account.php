7a0a280f0f2453a74509f4048716b3be9f50bce0
SarahRichou <85622707+SarahNR-coder@users.noreply.github.com>
2024-10-26 08:39:07 +0200

    new: branch signupsession & new: signup session

---
+++ account.php
@@ -0,0 +1,26 @@
+<?php
+//J'active la SESSION
+session_start();
+
+//Déclaration des variables d'affichage
+$pseudo = "";
+$email = "";
+$nameUser = "";
+$firstnameUser = "";
+$message = "";
+$classNavConnected = "toPage toggle";
+$classNavUnconnected ="displayNone";
+
+//AFFICHER LES DONNES DE L'UTILISATEURS  ENREGISTREES EN SESSION
+//1er Etape : je teste si j'ai bien des SESSIONS d'enregistrés
+if(isset($_SESSION['id_user'])){
+    //2nd Etape : je transmets les données de SESSIONS à mes variables d'affichages
+    $pseudo = $_SESSION['pseudo'];
+    $email = $_SESSION['email'];
+    $nameUser = $_SESSION['name_user'];
+    $firstnameUser = $_SESSION['firstname_user'];
+}
+
+
+include './views/view_account.php';
+?>
