d7c49f76ff0f0072fd68a2bcdb79a8f0149f85c3
SarahRichou <85622707+SarahNR-coder@users.noreply.github.com>
2024-11-26 22:34:27 +0100

    [view_wolf.php] Création + contenu statique HTML = il reste à faire CSS + contenu produit par php

---
+++ views/view_wolf.php
@@ -0,0 +1,26 @@
+<main>
+    <h1>Le Loup Gris</h1>
+    <section id="idCard">
+        <div id="tag">
+            CANIS LUPUS
+        </div>
+        <article id="infoCard">
+            <img src="./img/wolf.webp" alt="Loup.">
+            <div id="features">
+                <p><strong>Nom commun : </strong>Loup gris</p>
+                <p><strong>Classification : </strong>Mammifère, Carnivore, Famille des Canidés</p>
+                <p><strong>Habitat : </strong>Forêts, Toundras, Montagnes, Prairies</p>
+                <p><strong>Régime alimentaire : </strong>Carnivore, chasse principalement des grands mammifères,commeles cerfs et les wapitis</p>
+                <p><strong>Comportement social : </strong>Vivent en meutes hiérarchisées avec des structures sociales complexes</p>
+                <p><strong>Reproduction : </strong>Une portée par an, composée de 4 à 6 louveteaux en moyenne</p>
+                <p><strong>Statut de conservation : </strong>LC, Préoccupation mineure</p>
+            </div>
+        </article>
+    </section>
+    <section id="location">
+        <h2>LE LOUPS GRIS DANS LE PARC DU YELLOWSTONE</h2>
+        <img src="./img/wolf-animals-aera-map.webp" alt="Carte montrant la répartition faune dans le parc Yellowstone" />
+    </section>
+    <section id="comments">
+        <h2>COMMENTAIRES</h2>
+    </section>
