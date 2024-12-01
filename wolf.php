4524f41409e23d4d082bb41666da480a6fcef0e2
SarahRichou <85622707+SarahNR-coder@users.noreply.github.com>
2024-12-01 18:08:23 +0100

    wolf* ajout models, m.e.p fonctionnalité ajout commentaire

--- wolf.php
+++ wolf.php
@@ -1,11 +1,23 @@
 <?php
 
+include './models/model_users.php';
+include './models/model_comments.php';
+include './models/model_rating.php';
+include './models/model_page.php';
+include './utils/functions.php';
 
 session_start();
 
 $visibleConnectedUser ="";
 $visibleUnconnectedUser ="";
 $current_page = basename($_SERVER['PHP_SELF']);
+$listComments = "";
+$page = new ModelPage($current_page);
+$tabIdPage = $page->getPageIdFromName();
+$idPage = $tabIdPage[0];
+$date_commentaire = date("Y-m-d H:i:s");
+$idCommentateur = $_SESSION['id_user'];
+
 
 
 //Paramétrer l'affichage
@@ -15,6 +27,41 @@ if(isset($_SESSION['id_user'])){
 }else{
     $visibleConnectedUser = "displayNone";
     $visibleUnconnectedUser ="toggleElt";
+}
+
+function dataTestAddingComment(){
+    if(empty($_POST["contenuCommentaire"])){
+        return ["contenuCommentaire"=>'',"dateCommentaire"=>'',"idPage"=>'',"idCommentateur"=>'',"erreur"=>'Veuillez rédiger un commentaire'];
+    }
+
+    $current_page = basename($_SERVER['PHP_SELF']);
+    $page = new ModelPage($current_page);
+    $tabIdPage = $page->getPageIdFromName();
+    $idPage = $tabIdPage[0];
+    $contenu_commentaire = sanitize($_POST['contenuCommentaire']);
+    $date_commentaire = date("Y-m-d H:i:s");
+    $idCommentateur = $_SESSION['id_user'];
+
+
+    return ["contenuCommentaire"=>$contenu_commentaire,"dateCommentaire"=>$date_commentaire,"idPage"=>$idPage,"idCommentateur"=>$idCommentateur,"erreur"=>''];
+}
+
+//Test envoi formulaire ajout commentaire
+if(isset($_POST['Poster'])){
+    //test données 
+    $tab=dataTestAddingComment();
+
+    //test paramètre erreur pas vide càd erreur on ne récupère pas les données formulaire
+    if($tab['erreur'] != ''){
+        $message = $tab['erreur'];
+    }else{
+        $comment = new ModelComment();
+        
+
+        $comment->setContenuCommentaire($tab['contenuCommentaire'])->setDateCommentaire($tab['dateCommentaire'])->setIdPage($tab['idPage'])->setIdCommentateur($tab['idCommentateur']);
+
+        $message = $comment->addComment();      
+    }
 }
 
 
