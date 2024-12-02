848cd35ebbdd188e0dbccc4a7a0335e8a62329f3
SarahRichou <85622707+SarahNR-coder@users.noreply.github.com>
2024-12-02 12:05:04 +0100

    wolfphp* fonctionnalité ajout note focnctionnelle

--- wolf.php
+++ wolf.php
@@ -13,10 +13,8 @@ $visibleUnconnectedUser ="";
 $current_page = basename($_SERVER['PHP_SELF']);
 $listRatings = "";
 $listComments = "";
-$page = new ModelPage($current_page);
-$tabIdPage = $page->getPageIdFromName();
-$idPage = $tabIdPage[0];
-$date_commentaire = date("Y-m-d H:i:s");
+$messageComment = "";
+$messageRating = "";
 
 
 //Paramétrer l'affichage
@@ -53,52 +51,54 @@ if(isset($_POST['Poster'])){
 
     //test paramètre erreur pas vide càd erreur on ne récupère pas les données formulaire
     if($tab['erreur'] != ''){
-        $message = $tab['erreur'];
+        $messageComment = $tab['erreur'];
     }else{
         $comment = new ModelComment();
         
 
         $comment->setContenuCommentaire($tab['contenuCommentaire'])->setDateCommentaire($tab['dateCommentaire'])->setIdPage($tab['idPage'])->setIdCommentateur($tab['idCommentateur']);
 
-        $message = $comment->addComment();      
+        $messageComment = $comment->addComment();      
     }
 }
 
 
-// function dataTestAddingRating(){
-//     if(empty($_POST["contenuCommentaire"])){
-//         return ["contenuCommentaire"=>'',"dateCommentaire"=>'',"idPage"=>'',"idCommentateur"=>'',"erreur"=>'Veuillez rédiger un commentaire'];
-//     }
+function dataTestAddingRating(){
+    if(empty($_POST["nombreEtoiles"])){
+        return ["note"=>'',"dateNote"=>'',"idPage"=>'',"idNoteur"=>'',"erreur"=>'Veuillez donner une note'];
+    }
 
-//     $current_page = basename($_SERVER['PHP_SELF']);
-//     $page = new ModelPage($current_page);
-//     $tabIdPage = $page->getPageIdFromName();
-//     $idPage = $tabIdPage[0];
-//     $contenu_commentaire = sanitize($_POST['contenuCommentaire']);
-//     $date_commentaire = date("Y-m-d H:i:s");
-//     $idCommentateur = $_SESSION['id_user'];
+    //Récupérer l'identifiant correspondant à la page en base de données
+    $current_page = basename($_SERVER['PHP_SELF']);
+    $page = new ModelPage($current_page);
+    $tabIdPage = $page->getPageIdFromName();
+    $idPage = $tabIdPage[0];
+
+    $note = sanitize($_POST['nombreEtoiles']);
+    $date_note = date("Y-m-d H:i:s");
+    $idNoteur = $_SESSION['id_user'];
 
 
-//     return ["contenuCommentaire"=>$contenu_commentaire,"dateCommentaire"=>$date_commentaire,"idPage"=>$idPage,"idCommentateur"=>$idCommentateur,"erreur"=>''];
-// }
+    return ["note"=>$note,"dateNote"=>$date_note,"idPage"=>$idPage,"idNoteur"=>$idNoteur,"erreur"=>''];
+}
 
-// //Test envoi formulaire ajout commentaire
-// if(isset($_POST['Poster'])){
-//     //test données 
-//     $tab=dataTestAddingComment();
+//Test envoi formulaire ajout commentaire
+if(isset($_POST['Noter'])){
+    //test données 
+    $tab=dataTestAddingRating();
 
-//     //test paramètre erreur pas vide càd erreur on ne récupère pas les données formulaire
-//     if($tab['erreur'] != ''){
-//         $message = $tab['erreur'];
-//     }else{
-//         $comment = new ModelComment();
+    //test paramètre erreur pas vide càd erreur on ne récupère pas les données formulaire
+    if($tab['erreur'] != ''){
+        $messageRating = $tab['erreur'];
+    }else{
+        $rating = new ModelRating();
         
 
-//         $comment->setContenuCommentaire($tab['contenuCommentaire'])->setDateCommentaire($tab['dateCommentaire'])->setIdPage($tab['idPage'])->setIdCommentateur($tab['idCommentateur']);
+        $rating->setNote($tab['note'])->setDateNote($tab['dateNote'])->setIdPage($tab['idPage'])->setIdNoteur($tab['idNoteur']);
 
-//         $message = $comment->addComment();      
-//     }
-// }
+        $messageRating = $rating->addRating();      
+    }
+}
 
 include './views/view_header.php';
 include './views/view_wolf.php';
