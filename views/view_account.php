a01f562656fb983d22d2b92ea186b97a049a15be
SarahRichou <85622707+SarahNR-coder@users.noreply.github.com>
2025-01-12 15:10:44 +0100

    espaces

--- views/view_account.php
+++ views/view_account.php
@@ -1,26 +1,26 @@
-    <main>
-        <h1 id="title">Mon Compte</h1>
-        <section id="infosUtilisateur">
-        <h2 class="secondBestTitle">Mes Informations</h2>
-            <div id="imgProfil">
-                <img src="./img/image_profil_defaut.webp" alt="image de profil par défaut">
-            </div>
-            <div id="infosBasiques">
-                <p><span>Nom : </span><span class="info"><?php echo $nameUser ?></span></p>
-                <p><span>Prénom : </span><span class="info"><?php echo $firstnameUser ?></span></p>
-                <p><span>Adresse email : </span><span class="info"><?php echo $email ?></span></p>
-                <p><span>Pseudo: </span><span class="info"><?php echo $pseudo ?></span></p>
-            </div>
-        </section>
-        <section id="historiqueCommandes">
-            <h2 class="secondBestTitle">Mes Commandes</h2>
-        </section>
-        <section id="historiqueCommentaires">
-            <h2 class="secondBestTitle">Mes Commentaires</h2>
-            <?php echo $listComments ?>
-        </section>
-        <section id="historiqueNotes">
-            <h2 class="secondBestTitle">Mes Notes</h2>
-            <?php echo $listRatings ?>
-        </section>
-    </main>
+<main>
+    <h1 id="title">Mon Compte</h1>
+    <section id="infosUtilisateur">
+    <h2 class="secondBestTitle">Mes Informations</h2>
+        <div id="imgProfil">
+            <img src="./img/image_profil_defaut.webp" alt="image de profil par défaut">
+        </div>
+        <div id="infosBasiques">
+            <p><span>Nom : </span><span class="info"><?php echo $nameUser ?></span></p>
+            <p><span>Prénom : </span><span class="info"><?php echo $firstnameUser ?></span></p>
+            <p><span>Adresse email : </span><span class="info"><?php echo $email ?></span></p>
+            <p><span>Pseudo: </span><span class="info"><?php echo $pseudo ?></span></p>
+        </div>
+    </section>
+    <section id="historiqueCommandes">
+        <h2 class="secondBestTitle">Mes Commandes</h2>
+    </section>
+    <section id="historiqueCommentaires">
+        <h2 class="secondBestTitle">Mes Commentaires</h2>
+        <?php echo $listComments ?>
+    </section>
+    <section id="historiqueNotes">
+        <h2 class="secondBestTitle">Mes Notes</h2>
+        <?php echo $listRatings ?>
+    </section>
+</main>
--- views/view_index.php
+++ views/view_index.php
@@ -1,66 +1,65 @@
-          
-          <main>
-                <div class="imageInBg">
-                    <h1 class="textOverImage">
-                        YELLOWSTONE NATIONAL PARK
-                    </h1>
-                </div>
-                <section class="pagesMenu">
-                    <h2 class="uppage-title">Plan de visite du site : les pages détaillées</h2>
-                    <article class="pageTeaser  odd pageTeaserBg first">
-                        <div class="imageBox background">
-                            <a href="#"><img src="./img/home-symbole-NPS.webp" alt="Pointe de flèche, le symbole du National Park Service"></a>
-                        </div>
-                        <div class="textBox">
-                            <a href="#"><h3>A propos : le parc Yellowstone et le NPS</h3></a>
-                            <p>En apprendre plus à propos du parc et de l’agence fédérale qui le gère, parmis un grand nombre de parcs nationaux aux Etats Unis et dont la mission est la préservation et la transmission.</p>
-                        </div>
-                    </article>
-                    <article class="pageTeaser even">
-                        <div class="imageBox">
-                            <a href="#"><img src="./img/home-activites.webp" alt="Photographie de rafting"></a>
-                        </div>
-                        <div class="textBox">
-                            <a href="#"><h3>Les activités</h3></a>
-                            <p>Venez pratiquer une activité dans le parc Yellowstone. Nous vous proposons des itinéraires de randonnées ou de participer à des activités organisées</p>
-                        </div>
-                    </article>
-                    <article class="pageTeaser odd">
-                        <div class="imageBox">
-                            <a href="#"><img src="./img/home-yellowstone-entry.webp" alt="Photographie d'une entrée du parc de Yellowstone"></a>
-                        </div>
-                        <div class="textBox">
-                            <a href="#"><h3>Anticipez votre venue</h3></a>
-                            <p>Retrouvez des informations pour organiser votre venue, pour une visite sans imprévus. Vous trouverez tous les détails pratiques mais aussi une billeterie en ligne.</p>
-                        </div>
-                    </article>
-                    <article class="pageTeaser even pageTeaserBg">
-                        <div class="imageBox background">
-                            <a href="#"><img src="./img/home-key.webp" alt="Photographie d'une clé"></a>
-                        </div>
-                        <div class="textBox">
-                            <a href="./signin.php"><h3>Authentifiez-vous</h3></a>
-                            <p>En étant connecté sur le site vous bénificierez d’avantages, premier lieu la possibilité d’interagir avec le contenu en metant des notes et des commentaires.</p>
-                        </div>
-                    </article>
-                    <article class="pageTeaser odd">
-                        <div class="imageBox">
-                            <a href="#"><img src="./img/home-wolf.webp" alt="Photographie d'un loup"></a>
-                        </div>
-                        <div class="textBox">
-                            <a href="#"><h3>Faune, flore et autre</h3></a>
-                            <p>Venez apprécier la vie sauvage  mais aussi les phénomènes géologolique des lieux, en parcourant des fiches scientifiques détaillées.</p>
-                        </div>
-                    </article>
-                    <article class="pageTeaser even pageTeaserBg">
-                        <div class="imageBox background">
-                            <a href="#"><img src="./img/home-user.webp" alt="Image par défaut de compte utilisateur"></a>
-                        </div>
-                        <div class="textBox">
-                            <a href="./signup.php"><h3>Créez un compte</h3></a>
-                            <p>Inscrivez-vous. Vous trouverez dans votre compte vos informations, vos achats passés et toute votre activité : les notes et les commentaires.</p>
-                        </div>
-                    </article>
-                </section>
-            </main>
+<main>
+    <div class="imageInBg">
+        <h1 class="textOverImage">
+            YELLOWSTONE NATIONAL PARK
+        </h1>
+    </div>
+    <section class="pagesMenu">
+        <h2 class="uppage-title">Plan de visite du site : les pages détaillées</h2>
+        <article class="pageTeaser  odd pageTeaserBg first">
+            <div class="imageBox background">
+                <a href="#"><img src="./img/home-symbole-NPS.webp" alt="Pointe de flèche, le symbole du National Park Service"></a>
+            </div>
+            <div class="textBox">
+                <a href="#"><h3>A propos : le parc Yellowstone et le NPS</h3></a>
+                <p>En apprendre plus à propos du parc et de l’agence fédérale qui le gère, parmis un grand nombre de parcs nationaux aux Etats Unis et dont la mission est la préservation et la transmission.</p>
+            </div>
+        </article>
+        <article class="pageTeaser even">
+            <div class="imageBox">
+                <a href="#"><img src="./img/home-activites.webp" alt="Photographie de rafting"></a>
+            </div>
+            <div class="textBox">
+                <a href="#"><h3>Les activités</h3></a>
+                <p>Venez pratiquer une activité dans le parc Yellowstone. Nous vous proposons des itinéraires de randonnées ou de participer à des activités organisées</p>
+            </div>
+        </article>
+        <article class="pageTeaser odd">
+            <div class="imageBox">
+                <a href="#"><img src="./img/home-yellowstone-entry.webp" alt="Photographie d'une entrée du parc de Yellowstone"></a>
+            </div>
+            <div class="textBox">
+                <a href="#"><h3>Anticipez votre venue</h3></a>
+                <p>Retrouvez des informations pour organiser votre venue, pour une visite sans imprévus. Vous trouverez tous les détails pratiques mais aussi une billeterie en ligne.</p>
+            </div>
+        </article>
+        <article class="pageTeaser even pageTeaserBg">
+            <div class="imageBox background">
+                <a href="#"><img src="./img/home-key.webp" alt="Photographie d'une clé"></a>
+            </div>
+            <div class="textBox">
+                <a href="./signin.php"><h3>Authentifiez-vous</h3></a>
+                <p>En étant connecté sur le site vous bénificierez d’avantages, premier lieu la possibilité d’interagir avec le contenu en metant des notes et des commentaires.</p>
+            </div>
+        </article>
+        <article class="pageTeaser odd">
+            <div class="imageBox">
+                <a href="#"><img src="./img/home-wolf.webp" alt="Photographie d'un loup"></a>
+            </div>
+            <div class="textBox">
+                <a href="#"><h3>Faune, flore et autre</h3></a>
+                <p>Venez apprécier la vie sauvage  mais aussi les phénomènes géologolique des lieux, en parcourant des fiches scientifiques détaillées.</p>
+            </div>
+        </article>
+        <article class="pageTeaser even pageTeaserBg">
+            <div class="imageBox background">
+                <a href="#"><img src="./img/home-user.webp" alt="Image par défaut de compte utilisateur"></a>
+            </div>
+            <div class="textBox">
+                <a href="./signup.php"><h3>Créez un compte</h3></a>
+                <p>Inscrivez-vous. Vous trouverez dans votre compte vos informations, vos achats passés et toute votre activité : les notes et les commentaires.</p>
+            </div>
+        </article>
+    </section>
+</main>
 
